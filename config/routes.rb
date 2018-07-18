b7c55ff6a685d6a25532135218c8fc735c7ca694
Crystal Liu <shu.liu@wework.com>
2018-07-18 12:20:59 -0400

    add create request page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "requests#index"
+  get "/home" => "requests#index"
+
+  get "/create" => "requests#create"
 end
