eb826443ff52b6635b2ea4b5d1d9487bf621d1ca
minutetominute <3320594+minutetominute@users.noreply.github.com>
2018-07-18 14:32:21 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,74 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+num_users = 30
+num_requests = 10
+percent_completed = 0.20
+
+# user creation
+
+names = num_users.times.map do |idx|
+  Faker::Name.name
+end
+
+users = names.map do |name|
+  User.create!(name: name)
+end
+
+# request creation
+
+requests = num_users.times.map do |idx|
+  Request.create!(
+    user: users.sample,
+    description: Faker::TheITCrowd.quote
+  )
+end
+
+# offer creation
+
+requests.each do |request|
+  potential_offer_users = users - [request.user]
+
+  offer_users = offer_users.sample(
+    Random.rand((2..(offer_users.count/2)))
+  )
+
+  offers = offer_users.each do |offer_user|
+    Offer.create!(
+      user: user,
+      request: request,
+      karma_points: Random.rand((1..100)),
+      description: Faker::Hacker.say_something_smart
+    )
+  end
+
+  cancelled_offers = offers.sample(2)
+  cancelled_offers.each(&:cancel!)
+
+  rejected_offers = (offers - cancelled_offers).sample(2)
+  rejected_offers.each(&:timeout!)
+end
+
+# transaction creation
+
+requests.sample(requests.count * percent_completed).each do |request|
+  pending_offers = request.pending_offers
+
+  completed_offer = pending_offers.sample
+
+  completed_transaction = Transaction.create!(
+    offer: completed_offer
+  )
+  completed_transaction.complete!
+
+  cancelled_offer = (pending_offers - [completed_offer]).sample
+
+  cancelled_transaction = Transaction.create!(
+    offer: cancelled_offer
+  )
+
+  cancelled_transaction.cancel!
+end
