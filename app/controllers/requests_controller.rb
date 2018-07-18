cad007cd1aa4d746a0a9ae9e90c03a834f6df360
jennifer.yien <jenniferyien@users.noreply.github.com>
2018-07-18 11:33:01 -0400

    initial set up for requests page

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,10 @@
+class RequestsController < ApplicationController
+  def index
+    #this will be using the model Request.all for example
+    @requests = [
+      { name: "Request 1" },
+      { name: "Request 2" },
+      { name: "Request 3" }
+    ]
+  end
+end
