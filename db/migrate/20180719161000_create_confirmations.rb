ee4b8c06e2da2a945dc6d7703c35b8ec7d5dc432
minutetominute <3320594+minutetominute@users.noreply.github.com>
2018-07-19 12:28:32 -0400

    update schema and migration

--- db/migrate/20180719161000_create_confirmations.rb
+++ db/migrate/20180719161000_create_confirmations.rb
@@ -1,8 +1,8 @@
 class CreateConfirmations < ActiveRecord::Migration[5.1]
   def change
     create_table :confirmations do |t|
-      t.references :offerer, index: true, foreign_key: {to_table: :user}
-      t.references :requester, index: true, foreign_key: {to_table: :user}
+      t.references :offerer, index: true, foreign_key: {to_table: :users}
+      t.references :requester, index: true, foreign_key: {to_table: :users}
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180719160926) do
+ActiveRecord::Schema.define(version: 20180719161000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "confirmations", force: :cascade do |t|
+    t.bigint "offerer_id"
+    t.bigint "requester_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offerer_id"], name: "index_confirmations_on_offerer_id"
+    t.index ["requester_id"], name: "index_confirmations_on_requester_id"
+  end
 
   create_table "offers", force: :cascade do |t|
     t.bigint "user_id"
@@ -65,4 +74,6 @@ ActiveRecord::Schema.define(version: 20180719160926) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "confirmations", "users", column: "offerer_id"
+  add_foreign_key "confirmations", "users", column: "requester_id"
 end
