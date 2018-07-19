001d316e03b0b03d54c78e9178165b8f4fb7f5f7
Nora Lin <nora.p.lin@gmail.com>
2018-07-19 11:08:45 -0400

    Make create offer work

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,12 +1,33 @@
 class OffersController < ApplicationController
   before_action :current_user, :current_path
-  def create
+
+  def new
+    @service_request_id = params[:service_request_id]
     @offer = Offer.new
   end
 
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+
+    if @offer.save
+      redirect_to service_request_path(@offer.service_request)
+    else
+      render 'new'
+    end
+  end
+
   def cancel
-    Offer.find(params[:id]).cancel!
+    Offer.find(params[:offer_id]).cancel!
     redirect_to '/'
   end
 
+  private
+
+  def offer_params
+    params.require(:offer).permit(:karma_points, :description, :user_id, :service_request_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "service_requests#index"
 
-  resources :service_requests
+  #put "/offers/:id/cancel", to: "offers#cancel"
+  resources :service_requests do
+    resources :offers do
+      put 'cancel', to: 'offers#cancel'
+    end
+  end
 
-  get "/offer/new", to: "offers#create"
-  put "/offers/:id/cancel", to: "offers#cancel"
   get "/users/:id" => "users#show"
   get "/my_requests" => "users#my_requests"
   get "/my_offers" => "users#my_offers"
